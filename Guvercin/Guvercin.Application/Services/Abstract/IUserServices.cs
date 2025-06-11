0154d001e375ebcb99f1e1cfd474cfe7511345a5
Gökhan <151861553+gkayn9@users.noreply.github.com>
2025-06-11 17:48:28 +0300

    Add role management functionality with CreateRole and AddRoleToUser methods

--- Guvercin/Guvercin.Application/Services/Abstract/IUserServices.cs
+++ Guvercin/Guvercin.Application/Services/Abstract/IUserServices.cs
@@ -6,4 +6,6 @@ namespace Guvercin.Application.Services.Abstract;
 public interface IUserServices
 {
     Task<ResponseDto<object>> Register(RegisterDto registerDto);
+    Task<ResponseDto<object>> CreateRole(string roleName);
+    Task<ResponseDto<object>> AddToRole(string email, string roleName);
 }
